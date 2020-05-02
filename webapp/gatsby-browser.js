5d542c317c87c8b1d78a3fe0d57baa0487395d04
Tien Pham <tiendev94@gmail.com>
2020-05-02 20:52:37 +0300

    auth config

--- webapp/gatsby-browser.js
+++ webapp/gatsby-browser.js
@@ -5,11 +5,16 @@
  */
 
 // You can delete this file if you're not using it
-import React from 'react'
-import { ThemeProvider } from 'theme-ui'
+import React from "react"
+import { navigate } from "gatsby"
+import { AuthProvider } from "react-use-auth"
 
 export const wrapRootElement = ({ element }) => (
-    <ThemeProvider>
-        {element}
-    </ThemeProvider>
+  <AuthProvider
+    navigate={navigate}
+    auth0_domain="markdown-landing-page.eu.auth0.com"
+    auth0_client_id="Y1HjzCCRnnhkXpkhBCk6cxf31ikrPUt4"
+  >
+    {element}
+  </AuthProvider>
 )
