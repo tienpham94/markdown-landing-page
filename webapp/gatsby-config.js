5d817f370e3da257a36b0d0c90eb664e6d1f38c3
Tien Pham <tiendev94@gmail.com>
2020-05-02 19:35:39 +0300

    basic

--- webapp/gatsby-config.js
+++ webapp/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Markdown Landing Page`,
+    description: `Build a landing page for everything`,
+    author: `@tienpham94`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
